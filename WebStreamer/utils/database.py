5647f44dcabb09c1eb81c00f945caf7057ca6642
prad9036 <prad9036@gmail.com>
2025-02-19 21:17:08 +0530

    Update database.py
    Signed-off-by: prad9036 <prad9036@gmail.com>

--- WebStreamer/utils/database.py
+++ WebStreamer/utils/database.py
@@ -14,7 +14,7 @@ class Database:
         self.black = self.db.blacklist
         self.file = self.db.file
 
-    # ----------[User Management]----------
+    # ----------[Add User]----------
     def new_user(self, id):
         return dict(
             id=id,
@@ -27,47 +27,71 @@ class Database:
         user = self.new_user(id)
         await self.col.insert_one(user)
 
+    # ----------[User Info]----------
     async def get_user(self, id):
-        return await self.col.find_one({'id': int(id)})
+        user = await self.col.find_one({'id': int(id)})
+        return user
 
+    # ----------[User Count]----------
     async def total_users_count(self):
-        return await self.col.count_documents({})
+        count = await self.col.count_documents({})
+        return count
 
+    # ----------[User List]----------
+    async def get_all_users(self):
+        all_users = self.col.find({})
+        return all_users
+
+    # ----------[Remove User]----------
     async def delete_user(self, user_id):
         await self.col.delete_many({'id': int(user_id)})
 
+    # ----------[User Accept to TOS]----------
     async def agreed_tos(self, user_id):
         await self.col.update_one(
             {"id": int(user_id)},
             {"$set": {
                 "agreed_to_tos": True,
                 "when_agreed_to_tos": time.time()
-            }}
+                }
+            }
         )
 
-    # ----------[Ban System]----------
+    # ----------[Ban User]----------
+    def black_user(self, id):
+        return dict(
+            id=id,
+            ban_date=time.time()
+        )
+
     async def ban_user(self, id):
-        user = {"id": id, "ban_date": time.time()}
+        user = self.black_user(id)
         await self.black.insert_one(user)
         await self.delete_user(id)
 
+    # ----------[Unban User]----------
     async def unban_user(self, id):
         await self.black.delete_one({'id': int(id)})
 
+    # ----------[Check if User is Banned]----------
     async def is_user_banned(self, id):
-        return await self.black.find_one({'id': int(id)}) is not None
+        user = await self.black.find_one({'id': int(id)})
+        return True if user else False
 
+    # ----------[Banned User Count]----------
     async def total_banned_users_count(self):
-        return await self.black.count_documents({})
+        count = await self.black.count_documents({})
+        return count
 
-    # ----------[File System]----------
+    # ----------[Add File]----------
     async def add_file(self, file_info):
         file_info["time"] = time.time()
-        existing_file = await self.get_file_by_fileuniqueid(file_info["user_id"], file_info["file_unique_id"])
-        if existing_file:
-            return existing_file["_id"]
+        fetch_old = await self.get_file_by_fileuniqueid(file_info["user_id"], file_info["file_unique_id"])
+        if fetch_old:
+            return fetch_old["_id"]
         return (await self.file.insert_one(file_info)).inserted_id
 
+    # ----------[User File List]----------
     async def find_files(self, user_id, range):
         user_files = self.file.find({"user_id": user_id})
         user_files = user_files.skip(range[0] - 1).limit(range[1] - range[0] + 1)
@@ -75,8 +99,9 @@ class Database:
         total_files = await self.file.count_documents({"user_id": user_id})
         return user_files, total_files
 
-    async def search_files(self, user_id, query, page=1, limit=10):
-        """Search files with pagination."""
+    # ----------[Search Files]----------
+    async def search_files(self, user_id, query, pagination):
+        page, limit = pagination
         offset = (page - 1) * limit
 
         files_cursor = self.file.find(
@@ -87,8 +112,9 @@ class Database:
             {"user_id": user_id, "file_name": {"$regex": query, "$options": "i"}}
         )
 
-        return files_cursor, total_files, query  # Returning query to persist it
+        return files_cursor, total_files
 
+    # ----------[Get One File]----------
     async def get_file(self, _id):
         try:
             file_info = await self.file.find_one({"_id": ObjectId(_id)})
@@ -98,27 +124,39 @@ class Database:
         except InvalidId:
             raise FIleNotFound
 
+    # ----------[Get File Using Unique ID]----------
     async def get_file_by_fileuniqueid(self, id, file_unique_id, many=False):
         if many:
             return self.file.find({"file_unique_id": file_unique_id})
-        return await self.file.find_one({"user_id": id, "file_unique_id": file_unique_id})
+        else:
+            file_info = await self.file.find_one({"user_id": id, "file_unique_id": file_unique_id})
+        if file_info:
+            return file_info
+        return False
 
+    # ----------[Total File Count]----------
     async def total_files(self, id=None):
-        return await self.file.count_documents({"user_id": id} if id else {})
+        if id:
+            return await self.file.count_documents({"user_id": id})
+        return await self.file.count_documents({})
 
+    # ----------[Delete File]----------
     async def delete_one_file(self, _id):
         await self.file.delete_one({'_id': ObjectId(_id)})
 
+    # ----------[Update FileID List]----------
     async def update_file_ids(self, _id, file_ids: dict):
         await self.file.update_one({"_id": ObjectId(_id)}, {"$set": {"file_ids": file_ids}})
 
+    # ----------[Links Left]----------
     async def link_available(self, id):
         if not Var.LINK_LIMIT:
             return True
-        user = await self.get_user(id)
-        if user and user.get("Plan") == "Plus":
+        user = await self.col.find_one({"id": id})
+        if user.get("Plan") == "Plus":
             return "Plus"
-        elif user and user.get("Plan") == "Free":
+        elif user.get("Plan") == "Free":
             files = await self.file.count_documents({"user_id": id})
-            return files <= Var.LINK_LIMIT
-        return False
+            if files <= Var.LINK_LIMIT:
+                return True
+            return False
